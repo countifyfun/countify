825410062f079bb6c286446eea642d942bd31c84
ToastedToast <hey@toasted.dev>
2024-07-29 19:53:43 +0800

    chore(bot): remove console log from ping command

--- bot/src/commands/information/ping.ts
+++ bot/src/commands/information/ping.ts
@@ -5,7 +5,6 @@ import { api } from "../../utils/trpc";
 export default {
   data: new SlashCommandBuilder().setName("ping").setDescription("Ping!"),
   run: async ({ interaction }) => {
-    console.log(await api.getGuilds.query());
     interaction.reply(await api.greeting.query());
   },
 } satisfies Command;
