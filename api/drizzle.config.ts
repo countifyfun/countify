90a7e4be8719e8f8d45aa6d8b82f514d38aae461
ToastedToast <hey@toasted.dev>
2024-08-03 17:21:16 +0800

    feat(db): move to postgresql

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,14 +2,10 @@ import { apiEnv } from "@countify/env/api";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "mysql",
+  dialect: "postgresql",
   schema: "./src/utils/db/schema.ts",
   dbCredentials: {
-    host: apiEnv.DATABASE_HOST,
-    port: apiEnv.DATABASE_PORT,
-    user: apiEnv.DATABASE_USER,
-    password: apiEnv.DATABASE_PASSWORD,
-    database: apiEnv.DATABASE_NAME,
+    url: apiEnv.DATABASE_URL,
   },
   verbose: true,
 });
