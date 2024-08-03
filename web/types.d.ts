0b9fefb4dfb210c68647cbe89f44316c57fac717
ToastedToast <hey@toasted.dev>
2024-08-03 16:25:02 +0800

    fix types

--- web/types.d.ts
+++ web/types.d.ts
@@ -4,6 +4,6 @@ declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
-declare module "@/.map.ts" {
+declare module "@/.map" {
   export const map: Record<string, unknown>;
 }
