e03661870f40e9d68f203b46b1c4ecc74ea0929a
ToastedToast <hey@toasted.dev>
2024-08-08 22:13:42 +0800

    feat(web): add metadata

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import type { ReactNode } from "react";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,36 @@ const whitney = localFont({
   display: "swap",
   variable: "--font-whitney",
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://countify.fun"),
+  title: {
+    default: "Countify",
+    template: "%s — Countify",
+  },
+  description: "Your dream Discord counting bot.",
+  twitter: {
+    card: "summary_large_image",
+    creator: "@ToastedDev",
+    creatorId: "1145171094556426240",
+  },
+  openGraph: {
+    type: "website",
+    url: "/",
+    images: [
+      {
+        url: "https://countify.fun/og.jpg",
+      },
+    ],
+  },
+  icons: {
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    icon: [
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
